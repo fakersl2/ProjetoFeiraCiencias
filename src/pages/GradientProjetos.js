ee7c86e5cbf26043215e386c00ae2a3611b8e6fc
fakersl2 <projetotccfigma@outlook.com>
2024-09-27 12:52:58 -0300

    Update GradientProjetos.js

--- src/pages/GradientProjetos.js
+++ src/pages/GradientProjetos.js
@@ -6,7 +6,9 @@ const GradientProjetos = () => {
         <div
             className="z-0 pt-16 text-center text-white transition-all h-80 bg-gradient-to-b from-green-400 via-green-500 to-green-600">
             <div className="flex flex-col items-center py-10 mx-auto">
-                <FaArrowLeft className="p-4" />
+                <div className="">
+                <FaArrowLeft />
+                </div>
                 <h1 className="z-10 text-4xl font-extrabold sm:text-5xl dark:text-white">Nome do projeto</h1>
                 <p className="w-full max-w-4xl mt-2 text-center text-gray-200 sm:text-lg">
                     Aliquip nostrud tempor deserunt id ullamco pariatur eiusmod magna nisi ex in nostrud sunt.
