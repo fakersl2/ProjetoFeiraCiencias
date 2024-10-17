b6aab95fce398b0f3c0d80156c5101e4e65bea9b
fakersl2 <projetotccfigma@outlook.com>
2024-10-17 18:33:45 -0300

    Voltando arquivos

--- src/api/server.js
+++ src/api/server.js
@@ -1,28 +1,19 @@
-const express = require('express');
-const cors = require('cors');
-const path = require('path'); // Necessário para servir arquivos estáticos
-const routes = require('../api/routes/index.js');
+const express = require('express'); // Importa o Express
+const cors = require('cors'); // Importa o middleware cors
+const routes = require('../api/routes/index.js'); // Importa as rotas da aplicação
 
-const app = express();
-const port = process.env.PORT || 5000;
+const app = express(); // Cria uma instância do Express
+const port = 5000; // Define a porta do servidor
 
 // Configura o middleware CORS
 app.use(cors({
-    origin: '*' // Permite requisições de qualquer origem
+    origin: 'http://localhost:3000' // Permite apenas requisições da origem especificada
 }));
 
 app.use(express.json()); // Middleware para fazer o parsing de JSON no corpo das requisições
 routes(app); // Inicializa as rotas
 
-// Serve os arquivos estáticos da build do React
-app.use(express.static(path.join(__dirname, '../../pages/build')));
-
-// Rota para qualquer caminho não especificado (React router)
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../../pages/build', 'index.html'));
-});
-
 // Inicia o servidor na porta especificada
 app.listen(port, () => {
-    console.log(`Servidor rodando na porta ${port}`);
+    console.log(`Servidor rodando na porta ${port}`); // Log que indica que o servidor está ativo
 });
