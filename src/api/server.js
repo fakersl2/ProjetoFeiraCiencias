37d43d508e6fc118df8720ca9e343d30b48b3971
fakersl2 <projetotccfigma@outlook.com>
2024-10-17 16:15:45 -0300

    Preparando pra dar deploy no Vercel

--- src/api/server.js
+++ src/api/server.js
@@ -3,19 +3,17 @@ const cors = require('cors'); // Importa o middleware cors
 const routes = require('../api/routes/index.js'); // Importa as rotas da aplicação
 
 const app = express(); // Cria uma instância do Express
-const port = 5000; // Define a porta do servidor
+const port = process.env.PORT || 5000; // Usa a variável de ambiente PORT ou 5000 como padrão
 
 // Configura o middleware CORS
 app.use(cors({
-    origin: 'http://localhost:3000' // Permite apenas requisições da origem especificada
+    origin: '*' // Permite requisições de qualquer origem
 }));
 
 app.use(express.json()); // Middleware para fazer o parsing de JSON no corpo das requisições
 routes(app); // Inicializa as rotas
 
-module.exports = app;
-
 // Inicia o servidor na porta especificada
 app.listen(port, () => {
-    console.log(`Servidor rodando na porta ${port}`); // Log que indica que o servidor está ativo
+    console.log(`Servidor rodando na porta ${port}`);
 });
